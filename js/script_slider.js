36f3794e0a00adcb1161c17ac8c00de92bb5f83c
Semen <justpr0z.com.ru@gmail.com>
2023-01-07 18:32:12 +0300

    added all slider scripts

--- js/script_slider.js
+++ js/script_slider.js
@@ -1,18 +1,21 @@
 let images = [{
   url: "images/image-completed-projects1.png",
-  city: "Rostov-on-Don LCD admiral",
+  cityMain: "Rostov-on-Don, Admiral",
+  city: "Rostov-on-Don<br> LCD admiral",
   apartment: "81 m2",
   time: "3.5 months",
   cost: "Upon request"
 }, {
   url: "images/image-completed-projects2.png",
-  city: "Sochi Thieves",
+  cityMain: "Sochi Thieves",
+  city: "Sochi<br> Thieves",
   apartment: "105 m2",
   time: "4 months",
   cost: "Upon request"
 }, {
   url: "images/image-completed-projects3.png",
-  city: "Rostov-on-Don Patriotic",
+  cityMain: "Rostov-on-Don Patriotic",
+  city: "Rostov-on-Don<br> Patriotic",
   apartment: "93 m2",
   time: "3 months",
   cost: "Upon request"
@@ -25,11 +28,19 @@ function initSlider() {
   let sliderArrows = document.querySelector(".slider__arrows");
   let sliderDots = document.querySelector(".slider__dots")
   let sliderCity = document.querySelector(".slider__city")
+  let sliderCityProperty = document.querySelector(".slider__cityProperty")
+  let sliderApartmentProperty = document.querySelector(".slider__apartmentProperty")
+  let sliderTimeProperty = document.querySelector(".slider__timeProperty")
+  let sliderCostProperty = document.querySelector(".slider__costProperty")
 
   initImages();
   initArrows();
   initDots();
   initCity();
+  initCityProperty();
+  initApartmentProperty();
+  initTimeProperty();
+  initCostProperty();
 
   function initImages() {
     images.forEach((image, index) => {
@@ -70,7 +81,7 @@ function initSlider() {
 
   function initCity() {
     images.forEach((image, index) => {
-      let city = `<li class="main-completed-projects-item n${index} ${index === 0? "active" : ""}" data-index="${index}>${images[index].city}</li>`;
+      let city = `<li class="main-completed-projects-item n${index} ${index === 0? "active" : ""}" data-index="${index}">${images[index].cityMain}</li>`;
       sliderCity.innerHTML += city;
     });
     sliderCity.querySelectorAll(".main-completed-projects-item").forEach(city => {
@@ -82,6 +93,34 @@ function initSlider() {
     })
   }
 
+  function initCityProperty() {
+    images.forEach((image, index) => {
+      let cityProperty = `<span class="text3 n${index} ${index === 0? "active" : ""}" data-index="${index}">${images[index].city}</span>`;
+      sliderCityProperty.innerHTML += cityProperty;
+    });
+  }
+
+  function initApartmentProperty() {
+    images.forEach((image, index) => {
+      let apartmentProperty = `<span class="text3 n${index} ${index === 0? "active" : ""}" data-index="${index}">${images[index].apartment}</span>`;
+      sliderApartmentProperty.innerHTML += apartmentProperty;
+    });
+  }
+
+  function initTimeProperty() {
+    images.forEach((image, index) => {
+      let timeProperty = `<span class="text3 n${index} ${index === 0? "active" : ""}" data-index="${index}">${images[index].time}</span>`;
+      sliderTimeProperty.innerHTML += timeProperty;
+    });
+  }
+
+  function initCostProperty() {
+    images.forEach((image, index) => {
+      let costProperty = `<span class="text3 n${index} ${index === 0? "active" : ""}" data-index="${index}">${images[index].cost}</span>`;
+      sliderCostProperty.innerHTML += costProperty;
+    });
+  }
+
   function moveSlider(num) {
     sliderImages.querySelector(".active").classList.remove("active");
     sliderImages.querySelector(".n" + num).classList.add("active");
@@ -91,6 +130,18 @@ function initSlider() {
 
     sliderCity.querySelector(".active").classList.remove("active");
     sliderCity.querySelector(".n" + num).classList.add("active");
+
+    sliderCityProperty.querySelector(".active").classList.remove("active");
+    sliderCityProperty.querySelector(".n" + num).classList.add("active");
+
+    sliderApartmentProperty.querySelector(".active").classList.remove("active");
+    sliderApartmentProperty.querySelector(".n" + num).classList.add("active");
+
+    sliderTimeProperty.querySelector(".active").classList.remove("active");
+    sliderTimeProperty.querySelector(".n" + num).classList.add("active");
+
+    sliderCostProperty.querySelector(".active").classList.remove("active");
+    sliderCostProperty.querySelector(".n" + num).classList.add("active");
   }
 }
 
